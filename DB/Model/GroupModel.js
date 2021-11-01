f5b2460b3d635549b6a8eeb75c41c3d0631914d8
noel2017mdala <noelmdala2017@gmail.com>
2021-11-01 21:29:31 +0200

    add create group functionalities

--- DB/Model/GroupModel.js
+++ DB/Model/GroupModel.js
@@ -1,10 +1,54 @@
 const mongoose = require("mongoose");
 const GroupSchema = require("../Schema/GroupSchema");
 const Group = mongoose.model("Group", GroupSchema);
+const UserSchema = require("../Schema/UserSchema");
+const User = mongoose.model("User", UserSchema);
 
-const createGroup = (groupBody) => {
-  // let { fd } = groupBody;
-  // console.log(fd);
+const createGroup = async (groupBody) => {
+  let { description, groupUsers, groupImage, created_by } = groupBody;
+  if (
+    description !== "" &&
+    groupUsers !== "" &&
+    groupImage !== "" &&
+    created_by !== ""
+  ) {
+    let group = new Group({
+      groupName: description,
+      groupProfile: `http://localhost:8000/${groupImage}`,
+      groupUsers: groupUsers,
+      createdBy: created_by,
+    });
+    group = await group.save();
+
+    if (group) {
+      // let groupId = group._id;
+      let addUsersToGroup = Promise.all(
+        groupUsers.map(async (e) => {
+          let getUser = await User.findByIdAndUpdate(
+            e,
+            {
+              $addToSet: {
+                groups: group._id,
+              },
+            },
+            {
+              new: true,
+            }
+          );
+          if (getUser) {
+            return getUser;
+          }
+        })
+      );
+      let userGroup = await addUsersToGroup;
+      if (userGroup) {
+        return userGroup;
+      }
+      // let getUser = User.findById()
+    } else {
+      console.log("failed to create group");
+    }
+  }
 };
 
 module.exports = {
--- Routes/Group.js
+++ Routes/Group.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 const { createGroup } = require("../DB/Model/GroupModel");
 const GroupRouter = express.Router();
 
-let time = Date.now();
-
+/*
+- Sets the destination of the file to be uploaded
+*/
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "public/userProfiles");
   },
 
+  /*
+  - Renames the incoming file before being uploaded to the server
+  - this helps to easily find the file in the directory 
+  */
   filename: (req, file, cb) => {
-    cb(null, `${file.originalname}-${time}-${path.extname(file.originalname)}`);
+    let { Uid } = req.body;
+    cb(null, `${file.originalname}-${Uid}-${path.extname(file.originalname)}`);
   },
 });
 
 const upload = multer({
-  // dest: "public/userProfiles",
+  //Adding the storage object to Multer
   storage: storage,
+  // Declaring the max size of the uploaded image
   limits: {
     fileSize: 4000000,
   },
+
+  // restricting only images to be uploaded
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(png|jpeg|jpg)$/)) {
       return cb(new Error("Please upload an image"));
@@ -34,12 +44,38 @@ GroupRouter.post(
   "/createGroup",
   upload.single("file"),
   async (req, res) => {
-    // res.send();
-    console.log(
-      `http://localhost:8000/public/userProfiles/${
-        req.file.originalname
-      }-${time}-${path.extname(req.file.originalname)}`
-    );
+    //Parameters provided by the client
+    let { Uid, users, description, created_by } = req.body;
+    let userData = users.split(",");
+
+    // Renames the incoming file before being uploaded to the server
+    let icon = `public/userProfiles/${
+      req.file.originalname
+    }-${Uid}-${path.extname(req.file.originalname)}`;
+
+    /*
+Verifying if the image was uploaded before creating the group
+*/
+    if (fs.existsSync(icon)) {
+      let groupInfo = {
+        description,
+        groupUsers: userData,
+        groupImage: icon,
+        created_by,
+      };
+
+      // Pass the parameters to the createGroup for Group creation
+      let newGroup = await createGroup(groupInfo);
+      if (newGroup) {
+        res.status(200).send(newGroup);
+      } else {
+        res.status(400).json({
+          message: "Failed to add users to group",
+        });
+      }
+    } else {
+      console.log("The file is not available");
+    }
   },
   (error, req, res, next) => {
     res.status(400).json({ error: error.message });
